d2aa2f62db883484dbab8422cd3881ee8cc4585c
anangyoga <aharwinto@gmail.com>
2023-01-01 10:54:11 +0700

    fix: responsive list api component

--- src/components/ListAPI.jsx
+++ src/components/ListAPI.jsx
@@ -5,7 +5,7 @@ const ListAPI = ({ entries }) => {
   return (
     <div className="mx-2 mb-10">
       <h3 className="font-semibold text-xl text-gray-700/95 my-5">API Lists</h3>
-      <div className="grid gap-4 md: grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-3">
         {entries.map((item) => (
           <CardAPI item={item} key={item?.Description} />
         ))}
